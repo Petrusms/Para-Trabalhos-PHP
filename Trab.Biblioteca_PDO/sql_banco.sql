e6d67928e8239b3b2e032710049f8cfa7b2a9ad4
Petrusms <134728686+Petrusms@users.noreply.github.com>
2025-02-06 15:41:52 -0300

    Update sql_banco.sql

--- Trab.Biblioteca_PDO/sql_banco.sql
+++ Trab.Biblioteca_PDO/sql_banco.sql
@@ -3,6 +3,7 @@ use Biblioteca;
 
 create table Materiais_Leitura(
 	id int auto_increment primary key not null,
+    tipo varchar(1) NOT NULL,
     titulo varchar(50)not null,
     anoPublicacao int not null,
     categoria varchar(30)not null,
@@ -12,45 +13,30 @@ create table Materiais_Leitura(
     numCap int
 );
 
-create table Usuarios(
-    id int auto_increment primary key not null,
-    nome varchar(100) not null,
-    data_nascimento date not null,
-    CPF varchar(14) not null unique,
-    telefone varchar(15),
-    senha varchar(255) not null
-);
-
-
 -- Exemplos livros
-INSERT INTO Materiais_Leitura (titulo, anoPublicacao, categoria, numEdicao, editora, numPag, numCap) VALUES 
-("Dom Casmurro", 1899, "Romance", NULL, NULL, 320, 19),
-("O Alquimista", 1988, "Ficção/Fantasia", NULL, NULL, 208, 10),
-("A Moreninha", 1844, "Romance", NULL, NULL, 240, 12),
-("Grande Sertão: Veredas", 1956, "Romance Regionalista", NULL, NULL, 448, 22),
-("O Guarani", 1857, "Romance Histórico", NULL, NULL, 300, 15);
+INSERT INTO Materiais_Leitura (tipo, titulo, anoPublicacao, categoria, numEdicao, editora, numPag, numCap) VALUES 
+("L", "Dom Casmurro", 1899, "Romance", NULL, NULL, 320, 19),
+("L", "O Alquimista", 1988, "Ficção/Fantasia", NULL, NULL, 208, 10),
+("L", "A Moreninha", 1844, "Romance", NULL, NULL, 240, 12),
+("L", "Grande Sertão: Veredas", 1956, "Romance Regionalista", NULL, NULL, 448, 22),
+("L", "O Guarani", 1857, "Romance Histórico", NULL, NULL, 300, 15);
 
 -- Exemplos gibis
-INSERT INTO Materiais_Leitura (titulo, anoPublicacao, categoria, numEdicao, editora, numPag, numCap) VALUES 
-("Turma da Mônica", 2020, "Infantil", 100, NULL, NULL, NULL),
-("Batman: O Cavaleiro das Trevas", 1986, "Ação/Aventura", 1, NULL, NULL, NULL),
-("Mortal Kombat", 1994, "Ação", 5, NULL, NULL, NULL),
-("Asterix e Obelix", 1967, "Aventura/Comédia", 12, NULL, NULL, NULL),
-("Dragon Ball", 1984, "Ação/Fantasia", 20, NULL, NULL, NULL);
+INSERT INTO Materiais_Leitura (tipo, titulo, anoPublicacao, categoria, numEdicao, editora, numPag, numCap) VALUES 
+("G", "Turma da Mônica", 2020, "Infantil", 100, NULL, NULL, NULL),
+("G", "Batman: O Cavaleiro das Trevas", 1986, "Ação/Aventura", 1, NULL, NULL, NULL),
+("G", "Mortal Kombat", 1994, "Ação", 5, NULL, NULL, NULL),
+("G", "Asterix e Obelix", 1967, "Aventura/Comédia", 12, NULL, NULL, NULL),
+("G", "Dragon Ball", 1984, "Ação/Fantasia", 20, NULL, NULL, NULL);
 
 -- Exemplos revistas
-INSERT INTO Materiais_Leitura (titulo, anoPublicacao, categoria, numEdicao, editora, numPag, numCap) VALUES 
-("Veja", 2023,"Atualidades", 2500,"Abril",NULL,NULL),
-("Superinteressante",2023,"Ciência e Cultura",400,"Abril",NULL,NULL),
-("Época",2023,"Política e Economia",1000,"Globo",NULL,NULL),
-("Claudia",2023,"Moda e Comportamento",1000,"Abril",NULL,NULL),
-("Rolling Stone",2023,"Música e Cultura Pop",200,"Rolling Stone",NULL,NULL);
+INSERT INTO Materiais_Leitura (tipo, titulo, anoPublicacao, categoria, numEdicao, editora, numPag, numCap) VALUES 
+("R", "Veja", 2023,"Atualidades", 2500,"Abril",NULL,NULL),
+("R", "Superinteressante",2023,"Ciência e Cultura",400,"Abril",NULL,NULL),
+("R", "Época",2023,"Política e Economia",1000,"Globo",NULL,NULL),
+("R", "Claudia",2023,"Moda e Comportamento",1000,"Abril",NULL,NULL),
+("R", "Rolling Stone",2023,"Música e Cultura Pop",200,"Rolling Stone",NULL,NULL);
 
-INSERT INTO Usuarios (nome, data_nascimento, CPF, telefone, senha) VALUES 
-("Jorge", STR_TO_DATE("24/10/1998", "%d/%m/%Y"), "000.000.008-00", "(45) 99999-9999", "@jorge24"),
-("Maria", STR_TO_DATE("04/01/2000", "%d/%m/%Y"), "800.547.120-00", "(45) 99999-9990", "@maria04"),
-("João", STR_TO_DATE("03/09/2002", "%d/%m/%Y"), "800.549.119-00", "(45) 99999-9900", "@joAo03"),
-("Lucas", STR_TO_DATE("13/11/2005", "%d/%m/%Y"), "800.846.118-00", "(45) 99999-9000", "@Lucas13"),
-("Pedro", STR_TO_DATE("13/08/1990", "%d/%m/%Y"), "800.506.117-00", "(45) 99999-0000", "@PEDRO13");
-
+select * from Materiais_Leitura;
+-- drop database Biblioteca;
 
